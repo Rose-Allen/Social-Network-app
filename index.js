bf52b95b6b67be8d2b10dd3336dc43763c7d260a
Dimash <d.esengeldiev@mail.ru>
2022-09-11 19:36:44 +0600

    Основной Каркас

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(4444, (error) => {
+  if (error) {
+    return console.log(error);
+  }
+  console.log("СЕРВАК РАБОТАЕТ!");
+});
